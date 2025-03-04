8b299661e3e52a4fc7d17c3171e131fc0349ecd8
Kelvina <Kelvina-web.com>
2025-03-04 22:15:53 +0300

    Adding files

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+// Task 1: Declare customerName in global scope
+var customerName = 'bob';
+
+// Task 2: Write upperCaseCustomerName()
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Task 3: Write setBestCustomer()
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // Implicitly creates a global variable
+}
+
+// Task 4: Write overwriteBestCustomer()
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Task 5: Declare leastFavoriteCustomer and write changeLeastFavoriteCustomer()
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else'; // This will throw an error
+}
